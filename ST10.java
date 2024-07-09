ef8e2486bda79241a36452067d4ddcbaafec6e88
12345Jyoti <jyotikumarichaursiya@gmail.com>
2024-07-09 13:22:52 +0530

    Add Squre pattern

--- ST10.java
+++ ST10.java
@@ -2,9 +2,9 @@ public class ST10 {
     public static void main(String[] args) {
         int size = 5; // Change this value to create a larger or smaller square
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+        for (int i = 1; i <=size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1|| i == size || j == 1 || j == size) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
